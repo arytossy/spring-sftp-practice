e544397ca411b4b13b8f64a9085d8fd04dd9c7b9
WESERVE.CO.JP\arimitsu <toshihiko.arimitsu@weserve.co.jp>
2021-03-24 11:59:50 +0900

    Initailize project

---
+++ src/main/java/com/arytossy/sftp_practice/SftpPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.arytossy.sftp_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SftpPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SftpPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arytossy/sftp_practice/SftpPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arytossy.sftp_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SftpPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
